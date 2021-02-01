861391c219bb9c53d0549709c741f5c0f6de74aa
Kirill Peshin <kirill_peshin@epam.com>
2021-02-01 16:34:27 +0300

    Upd

---
+++ app/src/main/java/com/example/kotlinandroid/Calc.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinandroid
+
+class Calc {
+    fun add(a: Double, b: Double) : Double {
+        return a + b
+    }
+
+    fun subtract(a: Double, b: Double) : Double {
+        return a - b
+    }
+
+    fun multiply(a: Double, b: Double) : Double {
+        return a * b
+    }
+
+    fun divide(a: Double, b: Double) : Double {
+        return a / b
+    }
+}
