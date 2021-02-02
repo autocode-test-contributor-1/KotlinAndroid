40b0174d420738f8df8b3b785cbf3b6ff382c9a2
Kirill Peshin <kirill_peshin@epam.com>
2021-02-02 15:59:08 +0300

    Upd

--- app/src/androidTest/java/com/example/kotlinandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/kotlinandroid/ExampleInstrumentedTest.kt
@@ -1,8 +1,20 @@
 package com.example.kotlinandroid
 
+import android.view.View
+import android.widget.TextView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matcher
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,10 +25,46 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.kotlinandroid", appContext.packageName)
+    }
+
+    @Test
+    fun espressoTestResultIsDisplayed() {
+        onView(withText("Result is:")).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun espressoTest() {
+        val originalText = getText(withId(R.id.resultView))
+        onView(withId(R.id.firstNumber)).perform(clearText(), typeText("2"))
+        onView(withId(R.id.addButton)).perform(click())
+        assertNotEquals(originalText, getText(withId(R.id.resultView)))
+    }
+
+    fun getText(matcher: Matcher<View?>?): String? {
+        val stringHolder = arrayOf<String?>(null)
+        onView(matcher).perform(object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(TextView::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "getting text from a TextView"
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                val tv = view as TextView
+                stringHolder[0] = tv.text.toString()
+            }
+        })
+        return stringHolder[0]
     }
 }
